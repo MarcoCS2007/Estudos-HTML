9ae2706fb7367671722151a6931709e6b4810345
Marco Antônio Chaves De Souza <marcoantoniocs1000@gmail.com>
2024-12-13 15:09:27 -0300

    criado Classe Cachorro

--- Python POO/pessoa.py
+++ Python POO/pessoa.py
@@ -21,3 +21,59 @@ class Pessoa:
             return
         print(f'{self.nome} parou de comer')
         self.comendo=False
+
+
+class cachorro:
+    def __init__(self, nome, raça, idade, comer = False, brincar  = False, latir = False):
+        self.nome = nome
+        self.raça = raça
+        self.idade = idade
+        self.comer = comer
+        self.brincar = brincar
+        self.latir = latir
+
+    def comendo(self):
+        if self.comer:
+            print (f'{self.nome} já está comendo')
+            return
+        if self.brincar:
+            print (f'{self.nome} está brincando, ele não pode comer agora')
+            return
+        if self.latir:
+            print (f'{self.nome} parou de latir e foi comer')
+            self.latir = False
+            return
+        self.comer = True
+        print (f'{self.nome} está comendo')
+
+    def parar_comer(self):
+        if not self.comer:
+            print (f'{self.nome} não está comendo')
+            return
+        self.comer = False
+        print (f'{self.nome} parou de comer')
+
+    def brincando(self):
+        if self.brincar:
+             print (f'{self.nome} já está brincando')
+             return
+        if self.comer:
+            print (f'{self.nome} está comendo, não pode brincar agora')
+            return
+        if self.latir:
+            print (f'{self.nome} parou de latir e foi brincar')
+            self.latir = False
+        self.brincar = True
+        print (f'{self.nome} está brincando')
+
+    def parar_brincar(self):
+        if not self.brincar:
+            print (f'{self.nome} não está brincando')
+            return
+        self.brincar = False
+        print (f'{self.nome} parou de brincar')
+
+    def latindo(self):
+        print ('au au')
+        self.latir = True
+            
